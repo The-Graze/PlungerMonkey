6fd1f97cb4e7f49bd3fa841520e5631b70f7f1cd
Graze <Graze@DESKTOP-6KJ7H29>
2022-05-26 22:40:54 +0100

    Add project files.

---
+++ PlungerMonkey/PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace PlungerMonkey
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.graze.gorillatag.plungermonkey";
+        public const string Name = "PlungerMonkey";
+        public const string Version = "1.0.0";
+    }
+}
